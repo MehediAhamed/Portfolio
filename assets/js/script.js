41ee9d649655c96141fbc91c16be4d22f6ea79cd
MesTruck <mehedi47478@gmail.com>
2024-06-13 01:10:04 +0600

    New-3

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,56 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+
+
+      const url = './assets/CV/Mehedi Ahamed CV.pdf';
+    
+      // Asynchronous download of PDF
+      const loadingTask = pdfjsLib.getDocument(url);
+      loadingTask.promise.then(pdf => {
+        console.log('PDF loaded');
+    
+        // Fetch the first page
+        const pageNumber = 1;
+        pdf.getPage(pageNumber).then(page => {
+          console.log('Page loaded');
+    
+          const scale = 1.5;
+          const viewport = page.getViewport({ scale });
+    
+          // Prepare canvas using PDF page dimensions
+          const canvas = document.getElementById('pdf-canvas');
+          const context = canvas.getContext('2d');
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+    
+          // Render PDF page into canvas context
+          const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+          };
+          const renderTask = page.render(renderContext);
+          renderTask.promise.then(() => {
+            console.log('Page rendered');
+          });
+        });
+      }, reason => {
+        console.error('Error loading PDF: ' + reason);
+      });
+
+    
+
+
+
+
+
+
+
+  
+
+
+
+
+
